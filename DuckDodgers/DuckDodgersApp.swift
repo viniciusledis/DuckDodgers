069ace02bc071d7bd5f8cf0c0a2bb2028d4365de
Vinicius Ledis <tecnologia.jlrgrowth@gmail.com>
2024-10-18 19:21:11 -0300

    Initial Commit

---
+++ DuckDodgers/DuckDodgersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckDodgersApp.swift
+//  DuckDodgers
+//
+//  Created by Vinicius Ledis on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DuckDodgersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
